3fabdf70901ca2e90dc320d89b2081a0c423b59c
minjiyong@home <jiyong2528@gmail.com>
2024-09-24 11:49:58 +0900

    뼈대 완성(프린트로 무한루프 확인)

---
+++ character_move.py
@@ -0,0 +1,23 @@
+from pico2d import*
+
+open_canvas()
+
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
+def run_circle():
+        print('CIRCLE')
+        pass
+        
+
+def run_rectangle():
+        print('RECTANGLE')
+        pass
+
+
+while(True):
+        run_circle()
+        run_rectangle()
+
+
+close_canvas()
